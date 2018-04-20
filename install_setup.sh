0e8eb7b68546fb1ac31c52b23186f0dd11f2efb1
gongyuan <misschuer@163.com>
2018-04-20 17:51:38 +0800

    Update install_setup.sh

--- install_setup.sh
+++ install_setup.sh
@@ -1,84 +1,72 @@
-echo 'install yum-utils'
-yum install -y yum-utils
-
-yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
-# 更新源
-cat << EOF > /etc/yum.repos.d/mongodb-org-3.4.repo
-[mongodb-org-3.4]  
-name=MongoDB Repository  
-baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.4/x86_64/  
-gpgcheck=0  
-enabled=1  
-gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
-EOF
-
-yum update -y
-
-echo 'install git'
-yum install -y git
-
-# 安装openresty-resty 1.13.6
-echo 'install openresty-resty 1.13.6'
-yum install -y openresty-resty
-
-# 安装mongodb3.4
-echo 'install mongodb-org'
-yum install -y mongodb-org
-if [ ! -d "/data" ];then
-    mkdir /data
-fi
-if [ ! -d "/data/mongodb" ];then
-    mkdir /data/mongodb
-fi
-
-# 再另外一个终端输入 mongod --dbpath=/data/mongodb --port 27017
-# 加admin超级用户
-# mongo admin --eval "db.createUser({user:\"admin\", pwd:\"qwe123QWE\", roles:[{role:\"userAdminAnyDatabase\", db:\"admin\"}]})"
-
-# 加单个数据库的用户
-# mongo app --eval "db.createUser({user:\"miss\", pwd:\"chuer\", roles:[\"readWrite\", \"dbAdmin\"]})"
-
-# mongodb 加入 start_all.sh 和 stop_all.sh
-# start_all.sh
-#启动mongodb
-#if [[ -z `ps -ef|grep mongod|grep -v grep|awk {'print $2'}` ]]; then
-#    echo 'start mongodb'
-#    nohup mongod --dbpath=/data/mongodb --port 27017 --auth > /data/mongodb/log.out 2>&1 &
-#    sleep 2
-#fi
-
-# stop_all.sh
-# kill mongodb
-#mid=`ps -ef|grep mongod|grep -v grep|awk {'print $2'}`
-#if !([ -z "$mid" ];) then
-#    echo "killed mongod", $mid
-#    kill $mid
-#fi
-
-#安装redis4
-yum install -y gcc
-
-#安装wget
-yum install -y wget
-redis=redis-4.0.6
-wget http://download.redis.io/releases/$redis.tar.gz
-tar xzvf $redis.tar.gz
-cd $redis
-make MALLOC=libc
-cd src && make install
-
-cd ..
-# 配置redis启动环境
-if [ ! -d "/etc/redis" ];then
-    mkdir /etc/redis
-fi
-# 把redis.conf里面的daemonize 改成yes
-cp `pwd`/redis.conf /etc/redis/6379.conf
-cp `pwd`/utils/redis_init_script /etc/init.d/redisd
-#cd /etc/init.d
-#在redisd的前面加2行(#!/bin/sh后面)
-# chkconfig:   2345 90 10
-# description:  Redis is a persistent key-value database
-#chkconfig redisd on
-# 启动
-# service redisd start
++#!/bin/bash
++echo 'start setup'
++
++#创建一下log目录
++if [ ! -d "/home/log" ];then
++    mkdir /home/log
++fi
++
++#把必要的软件装一下
++echo 'install git'
++yum install git -y
++echo 'install pymongo'
++yum install pymongo -y
++
++#安装mongodb
++echo 'install mongodb'
++yum install mongodb -y
++yum install mongodb-server -y
++#初始化一下mongodb环境
++echo 'init mongodb'
++if [ ! -d "/data" ];then
++    mkdir /data
++fi
++if [ ! -d "/data/mongodb" ];then
++    mkdir /data/mongodb
++fi
++
++
++#把运维工具下载下来
++echo 'clone soft mgr'
++cd /home
++git clone https://misschuer@github.com/misschuer/soft_mgr.git
++
++#把运维工具下载下来
++echo 'clone openresty'
++cd /opt
++git clone https://misschuer@github.com/misschuer/openresty.git
++
++#把配置服下载下来
++echo 'clone conf_svr'
++cd /home
++git clone https://misschuer@github.com/misschuer/conf_svr.git
++
++#把跨服下载下来
++echo 'clone ext_web'
++cd /home
++git clone https://misschuer@github.com/misschuer/ext_web.git
++
++#开始安装服务器运行必须的一大堆东西了（网关啊，booost啊）
++echo 'install server mgr'
++cd /home
++git clone https://misschuer@github.com/misschuer/server-mgr.git
++#下载网关服和安全认证
++cd /home/server-mgr/bin
++chmod +x *
++#下载booost运行环境
++cd /home/server-mgr/lib
++chmod +x libboost_filesystem.so.1.55.0
++chmod +x libboost_regex.so.1.55.0
++chmod +x libboost_system.so.1.55.0
++#为操作系统建立booost链接
++cd /etc/ld.so.conf.d
++cat << EOF > cow.conf
++/home/server-mgr/lib/ 
++EOF
++ldconfig
++#下载c++动态库
++cd /home/server-mgr/lib
++#为c++动态库建立链接
++rm /usr/lib64/libstdc++.so.6
++cp /home/server-mgr/lib/libstdc++.so.6.0.17 /usr/lib64/libstdc++.so.6.0.17
++ln -s /usr/lib64/libstdc++.so.6.0.17 /usr/lib64/libstdc++.so.6
